a5e02c150d38e16daf1a4686d55f534273466e51
Felipe Moura Martins <felipemouramartins3@gmail.com>
2023-11-24 11:25:58 -0300

    Desafio

--- 10-reduce.js
+++ 10-reduce.js
@@ -1,5 +1,7 @@
 // 10-reduce.js
 
+import cursos from "./modulos/cursos.js";
+
 /* reduce (reduzir, redução)
 Passa por elementos de um array (usando uma função callback como parâmetros, e um parâmetro com valor inicial) 
 e retorna um único valor/resultado. Pode ser útil para operações matemáticas que geram um único resultado.
@@ -14,3 +16,36 @@ const total = valores.reduce((acumulador, valor) => {
 console.log(valores);
 console.log(total);
 
+// Sem reduce com for
+let acumuladora1 = 0;
+
+for( let i = 0; i < valores.length; i++ ){
+    // acumuladora1 = acumuladora1 + valores[i];
+    acumuladora1 += valores[i];
+}
+
+console.log(acumuladora1);
+
+// Sem reduce com for/of
+let acumuladora2 = 0;
+for( let valor of valores ){
+    // acumuladora2 = acumuladora2 + valor;
+    acumuladora2 += valor;
+}
+console.log(acumuladora2);
+
+console.log("-----------");
+
+const totalCurso = cursos.reduce((acumulador, valor) => {
+    return acumulador += valor.preco
+}, 0);
+
+console.log(totalCurso);
+
+// Desafio 
+
+const totalFrontBack = cursos
+.filter( curso => curso.categoria === "Front-End" || curso.categoria === "Back-End" )
+.reduce( (acumulador, curso) => { return acumulador + curso.preco}, 0)
+
+console.log(totalFrontBack);
