132d03b16c1bf4b7f5bba083c7e2b2e5d33867dd
Felipe Moura Martins <felipemouramartins3@gmail.com>
2023-11-28 11:36:59 -0300

    Correção usando map

---
+++ desafio-refatorado/desafio.js
@@ -0,0 +1,29 @@
+const botao = document.querySelector("#carregar");
+const divUsuarios = document.querySelector("#lista-de-usuarios");
+
+botao.addEventListener("click", async function(){
+    try {
+        const resposta = await fetch('https://jsonplaceholder.typicode.com/users');
+
+        if(!resposta.ok){
+            throw new Error(resposta.status);
+        }
+
+        const dados = await resposta.json();
+
+        if(!divUsuarios.innerHTML == ""){
+            dados.map(usuario => {
+                let secao = document.createElement("section");
+                secao.innerHTML = `
+                    <h2>${usuario.name}</h2>
+                    <p>${usuario.email}</p>
+                    <p>${usuario.website}</p>
+                    <hr>
+                `;
+                divUsuarios.appendChild(secao);
+            });
+        }
+    } catch (error) {
+        console.error("Erro: "+error.message);
+    }
+});
